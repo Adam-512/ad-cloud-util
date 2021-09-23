dc1e1ace473ed99f414ad8a198cfa345b4eeaecf
chenhui <512006634@qq.com>
2021-09-23 23:14:28 +0800

    初始

---
+++ README.md
@@ -0,0 +1,9 @@
+### Array 方法
+
+1.chunk 数组分块，返回二维数组
+
+2.compact 去除数组中的 false 值（不建议：直接使用 filter 替代）
+
+3.concat 拼接数组，参数 flat 一级（不建议）
+
+4.s
---
+++ chunk.js
@@ -0,0 +1,29 @@
+function chunk(arr = [], size = 1) {
+  let _size = +size;
+  //size>=1
+  //arr is array,length>0
+  let isArr = Array.isArray(arr);
+  let len = arr.length > 0;
+
+  let isNaN = Number.isNaN(_size);
+  let isSize = +size > 0;
+
+  if (!isArr || !len || isNaN || !isSize) {
+    return [];
+  }
+
+  let index = 0,
+    resIndex = 0,
+    resArr = [];
+
+  let chunkLen = Math.floor(arr.length / _size) || 1;
+  while (resIndex < chunkLen) {
+    resArr[resIndex] = arr.slice(index * size, size * (index + 1)); //0-3 3-6
+    resIndex++;
+    index++;
+  }
+  return resArr;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6], 3));
+console.log(chunk([], -1));
